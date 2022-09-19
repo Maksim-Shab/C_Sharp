cbbc4b0a6e0d36b742969b93cc896a74467cdfe5
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-19 18:38:29 +0700

    koren chisla

---
+++ seminar/seminar_1/chislo_kvadrata/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число квадрат: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введи число, которое в квадрате выдаст первое число: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number / number2 == number2)
+{
+    Console.WriteLine($"Всё верно!!! Квадратный корень числа {number} равен {number2}");
+}
+else{
+    Console.WriteLine($"Попробуй ещё раз!");
+}
