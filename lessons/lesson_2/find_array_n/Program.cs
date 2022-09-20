8ab641fb8b2ba5a54f774a7550904e117bea2d97
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-21 00:14:07 +0700

    find_array_n

---
+++ lessons/lesson_2/find_array_n/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {1,2,3,4,5,6,7,8,9,2,34,4,456734,74,8567,45236,};
+int n = array.Length;
+int find = 456734;
+int i = 0;
+
+while(i < n) {
+    if(array[i] == find) {
+        Console.WriteLine(i);
+        break;
+    }
+    i++;
+}
