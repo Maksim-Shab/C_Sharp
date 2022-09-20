4224081ec812ff9440621bf1914b41e308d37258
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-20 13:40:50 +0700

    last_digit

---
+++ seminar/seminar_1/last_digit/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите техзначное число: ");
+int num = int.Parse(Console.ReadLine());
+if(num / 100 < 1) {
+    Console.Write("Вы ввели не техзначное число");
+    return;
+} else if(num / 100 > 10) {
+    Console.Write("Вы ввели не техзначное число");
+    return;
+}
+Console.Write(num % 10);
+
+// Console.Clear();
+// Console.Write("Введите техзначное число: ");
+// int num = Convert.ToIn32(Console.ReadLine());
+
+// int num_dec = num - (num / 100) * 100;
+// int num_l = num - (num_dec / 10) * 10;
+
+// Console.Write(num_l);
