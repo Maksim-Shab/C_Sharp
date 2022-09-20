56898f527bb2521d2bcd4a4bc55e914d82a4efd3
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-20 12:26:33 +0700

    -n_to_n

---
+++ seminar/seminar_1/-n_to_n/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+double num_neg = -num;
+while(num_neg <= num) {
+    Console.Write(num_neg + " ");
+    num_neg++;
+}
