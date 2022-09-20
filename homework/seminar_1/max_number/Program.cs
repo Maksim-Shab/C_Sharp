eee304160d18bc8607c6e73225c786ecab54a93d
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-20 14:08:46 +0700

    max_number

---
+++ homework/seminar_1/max_number/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.Clear();
+Console.WriteLine("Введите два разных числа: ");
+int fierst_num = int.Parse(Console.ReadLine());
+int second_num = int.Parse(Console.ReadLine());
+int max = fierst_num;
+if(fierst_num < second_num) {
+    max = second_num;
+    int min = fierst_num;
+    Console.Write("Максимальное значение: " + max + " Минимальное значение: " + min);
+}
