423346ca90d9897ba21646e527e08ec7a5922728
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-20 14:41:31 +0700

    max_out_of_three

---
+++ homework/seminar_1/max_out_of_three/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход 
+// три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Введите три разных числа поочерёдно (после ввода кажого числа нажимай 'Ener'): ");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max =a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("Максимальное значение из веденных: " + max);
