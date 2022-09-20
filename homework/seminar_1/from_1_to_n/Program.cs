6f6dd7974d703facad3d580c9fbf69b875335d58
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-20 17:25:59 +0700

    from_1_to_n

---
+++ homework/seminar_1/from_1_to_n/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введи число и я выведу тебе все четные значения этого числа: ");
+double num = Convert.ToDouble(Console.ReadLine());
+double num_new = 2;
+while(num_new <= num) {
+    if(num_new % 2 == 0)
+    Console.Write(num_new + " ");
+    num_new++;
+}
