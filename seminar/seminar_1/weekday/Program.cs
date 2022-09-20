5b822463c524156fe9c6334b24f401814228f047
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-20 12:00:16 +0700

    weekday

--- seminar/seminar_1/weekday/Program.cs
+++ seminar/seminar_1/weekday/Program.cs
@@ -1,3 +1,30 @@
 ﻿Console.Clear();
+
 Console.WriteLine("Введите номер дня недели: ");
-Console
+
+int numWeekday = int.Parse(Console.ReadLine());
+
+if(numWeekday == 1) {
+    Console.WriteLine("Понедельник");
+}
+else if(numWeekday == 2) {
+    Console.WriteLine("Вторник");
+}
+else if(numWeekday == 3) {
+    Console.WriteLine("Среда");
+}
+else if(numWeekday == 4) {
+    Console.WriteLine("Четверг");
+}
+else if(numWeekday == 5) {
+    Console.WriteLine("Пятница");
+}
+else if(numWeekday == 6) {
+    Console.WriteLine("Суббота");
+}
+else if(numWeekday == 7) {
+    Console.WriteLine("Воскресенье");
+}
+else {
+    Console.WriteLine("Введи число от 1 до 7 ! Попробуй ещё раз.");
+}
