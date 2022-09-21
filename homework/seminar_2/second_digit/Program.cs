37035879971d2404fc811776e771bfcfea752a84
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-22 00:50:32 +0700

    second_digit

---
+++ homework/seminar_2/second_digit/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+if(num / 100 < 1) {
+    Console.Write("Вы ввели не техзначное число");
+    return;
+} else if(num / 100 > 10) {
+    Console.Write("Вы ввели не техзначное число");
+    return;
+}
+string n = num.ToString();
+Console.Write("Вторая цыфра введенного числа: " + n[1]);
