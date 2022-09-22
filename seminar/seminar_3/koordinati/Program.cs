3c06d1819dd969a466e3d81faab2e18fbea95ec1
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-23 01:11:54 +0700

    dve zalachi

---
+++ seminar/seminar_3/koordinati/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите x:");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y:");
+int y = int.Parse(Console.ReadLine());
+
+void FindChevert(int x, int y){
+    
+if(x > 0 && y > 0) {
+   return Console.WriteLine("1");
+}
+if(x < 0 && y > 0) {
+   return Console.WriteLine("2");
+}
+if(x < 0 && y < 0) {
+   return Console.WriteLine("4");
+}
+else {
+   return Console.WriteLine("1");
+}
+}
