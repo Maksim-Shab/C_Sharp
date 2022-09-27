43187b784bb930ffd3e914eed9d8d7a819b5bb92
Maksim-Shab <shab.m.p@yandex.ru>
2022-09-27 17:58:42 +0700

    sem_3

--- homework/seminar_3/seminar_3/Program.cs
+++ homework/seminar_3/seminar_3/Program.cs
@@ -1,35 +1,41 @@
-﻿int name = 19;
+﻿// Введите номер задачи!
+int name = 19;
 
 switch(name) {
     case (19):
     // Напишите программу, которая принимает на вход пятизначное число и проверяет,
     // является ли оно палиндромом.
-
         Console.Clear();
         Console.WriteLine("Введите пятизначное число: ");
         int num = int.Parse(Console.ReadLine());
         string s = num.ToString();
-        int i = 0;
-        int mas = s[i];
-        int n = s.Length;
-        // Console.WriteLine(s.Length);
+        int n = s.Length - 1;
+        int j = 0;
         if(s.Length == 5) {
-            if(s[i] != s[n - 1]) {
+            for(int i = 0; i < s.Length / 2; i++) {
+                if(s[i] != s[n]) {
+                    j = 1;
+                } 
+                n--;                    
+            }
+            if(j == 1){
                 Console.WriteLine("Введеное число не является палиндромом!");
-            } 
-            else {
-                while(i < n / 2) {
-                    if(s[i] == s[n - 1]) {
-                        i++;
-                        n--;
-                    }
-                }
-            Console.WriteLine("Введеное число является палиндромом!");     
-            }     
+            } else {
+                Console.WriteLine("Введеное число является палиндромом!");        
+            }
         } else {
         Console.WriteLine("Введено не пятизначное число!");
         }
 
+       
+
 
     break;    
+
+
+    case (21):
+
+
+
+    break;
 }
