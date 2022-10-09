031c0387d5cbd2f212ab47120d21fca7bdd704db
Maksim-Shab <shab.m.p@yandex.ru>
2022-10-09 20:35:27 +0700

    hw seminar_6_4

--- homework/seminar_6/seminar_6/Program.cs
+++ homework/seminar_6/seminar_6/Program.cs
@@ -1,6 +1,6 @@
 ﻿// seminar_6
 
-int name = 43;
+int name = 45;
 
  switch(name) {
 
@@ -71,6 +71,79 @@ int name = 43;
     double y = k2 * x + b2;
 
     Console.WriteLine("Прямые пересекаються в точке с координатами Х: " + x + ", Y: " + y);
+
+    break;
+
+    case(44):
+
+    // Напишите программу, которая задаёт массив из n элементов,
+    // которые необходимо заполнить случайными значениями
+    // и сдвинуть элементы массива влево, или вправо на 1 позицию.
+    // [8, 5, 1, 7, 0] - [5, 1, 7, 0, 8] - сдвиг влево
+    // [8, 5, 1, 7, 0] - [0, 8, 5, 1, 7] - сдвиг вправо
+
+    Console.Clear();
+
+    int[] array1 = new int[5];
+    for (int i = 0; i < array1.Length; i++)
+    {
+        array1[i] = new Random().Next(1, 10);
+    }
+    Console.WriteLine("[" + String.Join(",", array1) + "]");
+    Console.WriteLine("Если нужно сдвинуть влево, введите '1', ");
+    Console.WriteLine("ИЛИ");
+    Console.Write("Если нужно сдвинуть влево, введите '2': ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    if(number1 == 1){
+        int j = 0;
+        for (int i = 0; i < array1.Length - 1; i++)
+        {
+           j = array1[i];
+           array1[i] = array1[i + 1];
+           array1[i + 1] = j;
+        }
+        Console.WriteLine("[" + String.Join(",", array1) + "]");
+    } 
+    else if(number1 == 2) {
+        int h = 0;
+        for (int i = array1.Length - 1; i > 0 ; i--)
+        {
+            h = array1[i];
+            array1[i] = array1[i - 1];
+            array1[i - 1] = h;
+        }
+        Console.WriteLine("[" + String.Join(",", array1) + "]");
+    }
+
+    break;
+
+    case(45):
+
+    // Напишите программу, которая задаёт массив из n элементов,
+    // которые необходимо заполнить случайными значениями
+    // и определить существует ли пара соседних элементов
+    // с одинаковыми значениями, при наличии такого элемента заменить его
+    // на уникакальное значение.
+    // [1,2,3,3] -> [1,2,3,4]
+
+    Console.Clear();
+
+    System.Console.WriteLine("Введите длину массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[] array5 = new int[n];
+    for (int i = 0; i < n - 1; i++)
+    {
+        array5[i] = new Random().Next(1, 9);
+    }
+    System.Console.WriteLine("[" + String.Join(",", array5) + ']');
+
+    for (int i = 0; i < array5.Length - 1; i++)
+    {
+        if(array5[i] == array5[i + 1]) {
+            array5[i + 1] = new Random().Next(1,9);
+        }
+    }
+    System.Console.WriteLine("[" + String.Join(",", array5) + ']');
 
     break;
  }
